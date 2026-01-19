fea39c6cbccc9133105df19010506326fb4cff4e
Victoria Vielma <victoriasaleck@gmail.com>
2026-01-19 08:27:26 -0500

    Hola mundo

---
+++ ppt.js
@@ -0,0 +1,19 @@
+
+// variabeles
+let jugador = 0
+let pc = 2 
+jugador = prompt("Eije: 1 para piedra 2 2 para papel, 3 para tijera")
+// alert("Elegiste " + jugador)
+
+if (juagdor == 1) {
+    alert("Elegiste priedra")
+}
+if (juagdor == 2) {
+    alert("Elegiste papel")
+}
+if (juagdor == 3) {
+    alert("Elegiste tijera")
+}
+
+
+
