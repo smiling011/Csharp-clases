5f272f56238931eb72ec246a32ac2dbd41048517
Victoria Vielma <victoriasaleck@gmail.com>
2026-01-20 10:04:27 -0500

    Trabajo extra

---
+++ Trabajo en Visual Studio .net y c#/Strings/Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vicky ID");
+
+            int altura = 168;
+            int edad = 89;
+            string name = "Vicky";
+            String info = "Nacio en NY, estudia ingeniería";
+            var hobby = "deportista";
+
+            string tarjetaId = $"La informacion de Vicky es la siguiente su edad s 89 años, su aluraq es 168";
+            Console.WriteLine(info  );
+
+            Console.ReadKey();
+
+        }
+    }
+}
